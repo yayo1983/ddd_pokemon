3d329ba1b3d255e4866c6a99f5b5a3e99e5b7169
yayo1983 <yazanenator@gmail.com>
2024-06-29 00:12:04 -0600

    Agregado inyección de dependencias

--- pokemon/application/views.py
+++ pokemon/application/views.py
@@ -2,16 +2,21 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from pokemon.application.serializers import PokemonSerializer
-from pokemon.application.abstract_factory import PokemonFactory
+from pokemon.application.abstract_factory import PokemonFactory, ServiceFactory
+
 
 class PokemonView(APIView):
-    
+    def __init__(self, factory: ServiceFactory = None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.factory = factory or PokemonFactory()
+
     def get(self, request, name_id):
         try:
-            factory = PokemonFactory()
-            service = factory.create_service()
+            service = self.factory.create_service()
             pokemon = service.get_pokemon_abilities(name_id)
             serializer = PokemonSerializer(pokemon)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except ValueError:
-            return Response({"error": "Pokemon not found"}, status=status.HTTP_404_NOT_FOUND)
+            return Response(
+                {"error": "Pokemon not found"}, status=status.HTTP_404_NOT_FOUND
+            )
--- pokemon/domain/services.py
+++ pokemon/domain/services.py
@@ -11,18 +11,18 @@ class PokemonService:
      def get_pokemon_abilities(self, name_id: str) -> Pokemon:
         try:
             response = requests.get(f"{POKEAPI_URL}{name_id}")
-            response.raise_for_status()  # Esto lanza un error para códigos de estado no exitosos
+            response.raise_for_status()  
             data = response.json()
             abilities = [ability['ability']['name'] for ability in data['abilities']]
             return Pokemon(name_id=name_id, abilities=abilities)
         except requests.exceptions.HTTPError as http_err:
-            raise ValueError(f"HTTP error occurred: {http_err}")  # Maneja errores HTTP específicos
+            raise ValueError(f"HTTP error occurred: {http_err}")  
         except requests.exceptions.RequestException as req_err:
-            raise ValueError(f"Request error occurred: {req_err}")  # Maneja errores de solicitud
+            raise ValueError(f"Request error occurred: {req_err}")  
         except KeyError as key_err:
-            raise ValueError(f"Unexpected response format: {key_err}")  # Maneja errores de formato de respuesta
+            raise ValueError(f"Unexpected response format: {key_err}")  
         except Exception as e:
-            raise ValueError(f"An error occurred: {e}")  # Maneja otros errores genéricos
+            raise ValueError(f"An error occurred: {e}")  
 
             
 
