c2553d96eaf44a9a7be566f209af96fa0d702643
yayo1983 <yazanenator@gmail.com>
2024-07-11 22:34:47 -0600

    better sintaxis i service class

--- pokemon/domain/services.py
+++ pokemon/domain/services.py
@@ -5,23 +5,24 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-POKEAPI_URL = os.getenv("POKEAPI_URL")
-
 
 class PokemonService:
+    def __init__(self):
+        self.poke_api_url = os.getenv("POKEAPI_URL")
+
     def get_pokemon_abilities(self, name_id: str) -> Pokemon:
         try:
-            response = requests.get(f"{POKEAPI_URL}{name_id}")
+            response = requests.get(f"{self.poke_api_url}{name_id}", timeout=5)
             if response.status_code != 200:
                 raise ValueError(f"An error occurred in status: {response.status_code}")
             data = response.json()
             abilities = [ability["name"] for ability in data["results"]]
             return Pokemon(name_id=name_id, abilities=abilities)
         except requests.exceptions.HTTPError as http_err:
-            raise ValueError(f"HTTP error occurred: {http_err}")
+            raise ValueError(f"HTTP error occurred: {http_err}") from http_err
         except requests.exceptions.RequestException as req_err:
-            raise ValueError(f"Request error occurred: {req_err}")
+            raise ValueError(f"Request error occurred: {req_err}") from req_err
         except KeyError as key_err:
-            raise ValueError(f"Unexpected response format: {key_err}")
+            raise ValueError(f"Unexpected response format: {key_err}") from key_err
         except Exception as e:
-            raise ValueError(f"An error occurred: {e}")
+            raise ValueError(f"An error occurred: {e}") from e
